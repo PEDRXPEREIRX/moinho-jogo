c524795c60f55c2e8e5ea1581c61bd9bee0b54cd
pedro <pedrobilharpereira@gmail.com>
2026-04-12 23:00:58 -0300

    termino codigo, falta testar

--- moinho.c
+++ moinho.c
@@ -1,23 +1,5 @@
 #include <stdio.h>
 
-void imprimir_tabuleiro(void) {
-    printf("\n          M O I N H O         \n\n");
-    printf("  0------------1------------2\n");
-    printf("  |            |            |\n");
-    printf("  |   3--------4--------5   |\n");
-    printf("  |   |        |        |   |\n");
-    printf("  |   |   6----7----8   |   |\n");
-    printf("  |   |   |         |   |   |\n");
-    printf("  9--10--11        12--13--14\n");
-    printf("  |   |   |         |   |   |\n");
-    printf("  |   |  15---16---17   |   |\n");
-    printf("  |   |        |        |   |\n");
-    printf("  |  18--------19-------20  |\n");
-    printf("  |            |            |\n");
-    printf(" 21-----------22-----------23\n\n");
-    printf("Jogador A: X\nJogador B: O\n\n");
-}
-
 int tabuleiro[24] = {0};
 
 int vizinhos[24][4] = {
@@ -47,12 +29,6 @@ int vizinhos[24][4] = {
     {22, 14, -1, -1},
 };
 
-int vizinhos_validos(int vem, int vai){
-    for(int i=0; i<4; i++)
-        if(vizinhos[vem][i] == vai)
-            return 1;
-    return 0;
-}
 
 int moinhos[16][3] = {
     {0,  1,  2},
@@ -73,6 +49,62 @@ int moinhos[16][3] = {
     {2, 14, 23},
 };
 
+char simbolo(int pos){
+    if(tabuleiro[pos] == 1)
+        return 'X';
+    if(tabuleiro[pos] == 2)
+        return 'O';
+    return '@';
+}
+
+void imprimir_tabuleiro(void) {
+    printf("\n          M O I N H O\n\n");
+    printf("  %c------------%c------------%c\n", simbolo(0), simbolo(1), simbolo(2));
+    printf("  |            |            |\n");
+    printf("  |   %c--------%c---------%c  |\n", simbolo(3), simbolo(4), simbolo(5));
+    printf("  |   |        |        |   |\n");
+    printf("  |   |   %c----%c----%c   |   |\n", simbolo(6), simbolo(7), simbolo(8));
+    printf("  |   |   |         |   |   |\n");
+    printf("  %c---%c---%c         %c---%c---%c\n", simbolo(9), simbolo(10), simbolo(11), simbolo(12), simbolo(13), simbolo(14));
+    printf("  |   |   |         |   |   |\n");
+    printf("  |   |   %c----%c----%c   |   |\n", simbolo(15), simbolo(16), simbolo(17));
+    printf("  |   |        |        |   |\n");
+    printf("  |   %c--------%c---------%c  |\n", simbolo(18), simbolo(19), simbolo(20));
+    printf("  |            |            |\n");
+    printf("  %c------------%c------------%c\n\n", simbolo(21), simbolo(22), simbolo(23));
+    printf("  Jogador 1: X    Jogador 2: O\n\n");
+
+    printf("          REFERENCIA\n\n");
+    printf("  0------------1------------2\n");
+    printf("  |            |            |\n");
+    printf("  |   3--------4--------5   |\n");
+    printf("  |   |        |        |   |\n");
+    printf("  |   |   6----7----8   |   |\n");
+    printf("  |   |   |         |   |   |\n");
+    printf("  9---10--11        12--13--14\n");
+    printf("  |   |   |         |   |   |\n");
+    printf("  |   |   15---16---17   |   |\n");
+    printf("  |   |        |        |   |\n");
+    printf("  |   18-------19-------20   |\n");
+    printf("  |            |            |\n");
+    printf("  21-----------22-----------23\n\n");
+}
+
+int posicao_valida(int pos){
+    return (pos >=0 && pos <= 23);
+}
+
+int posicao_vazia(int pos){
+    return (tabuleiro[pos] == 0);
+}
+
+int vizinhos_validos(int vem, int vai){
+    for(int i=0; i<4; i++)
+        if(vizinhos[vem][i] == vai)
+            return 1;
+    return 0;
+}
+
 int moinho_valido(int jogador, int pos){
     for(int i=0; i<16; i++)
         for(int j=0; j<3; j++)
@@ -89,16 +121,34 @@ int todas_no_moinho(int oponente){
     return 1;
 }
 
-int posicao_valida(int pos){
-    return (pos >=0 && pos <= 23);
+int bloqueado(int jogador){
+    for(int i=0; i<24; i++)
+        if(tabuleiro[i] == jogador)
+            for(int j=0; j<4; j++)
+                if(vizinhos[i][j] != -1 && posicao_vazia(vizinhos[i][j]))
+                    return 0;
+    return 1;
 }
 
-int posicao_vazia(int pos){
-    return (tabuleiro[pos] == 0);
+int verificar_vitoria(void){
+    int cont1=0, cont2=0;
+    for(int i=0; i<24; i++){
+        if(tabuleiro[i] == 1)
+            cont1++;
+        if(tabuleiro[i] == 2)
+            cont2++;
+    }
+    if(cont1<3 || bloqueado(1))
+        return 2;
+    else if(cont2<3 || bloqueado(2))
+        return 1;
+    else
+        return 0;
 }
 
-void colocar_peca(int jogador){
+int colocar_peca(int jogador){
     int pos;
+
     do{
         imprimir_tabuleiro();
 
@@ -113,6 +163,7 @@ void colocar_peca(int jogador){
     } while(!posicao_valida(pos) || !posicao_vazia(pos));
 
     tabuleiro[pos] = jogador;
+    return pos;
 }
 
 void remover_peca(int jogador){
@@ -137,27 +188,56 @@ void remover_peca(int jogador){
     tabuleiro[pos] = 0;
 }
 
-int main(){
-    imprimir_tabuleiro();
-    return 0;
+int mover_peca(int jogador){
+    int posA, posB;
+
+    do{
+        imprimir_tabuleiro();
+
+        printf("Jogador %d, escolha uma peca para mover: ", jogador);
+        scanf("%d", &posA);
+        printf("Agora escolha uma posicao valida: ");
+        scanf("%d", &posB);
+
+        if(!posicao_valida(posA))
+            printf("Posicao invalida! Escolha outra: \n");
+        else if(!vizinhos_validos(posA, posB))
+            printf("Nao e vizinho! Escolha outra: \n");
+        else if(tabuleiro[posA] != jogador)
+            printf("Nao tem peca na posicao para troca! Escolha outra: ");
+        else if(!posicao_vazia(posB))
+            printf("Posicao ocupada! Escolha outra: \n");
+    } while(!posicao_valida(posA) || !vizinhos_validos(posA, posB) || tabuleiro[posA] != jogador || !posicao_vazia(posB));
+    tabuleiro[posB] = jogador;
+    tabuleiro[posA] = 0;
+    return posB;
 }
 
+int main(){
+    int jogador = 1, pos, vencedor;
 
+    for(int i=0; i<18; i++){
+        pos = colocar_peca(jogador);
+        if(moinho_valido(jogador, pos))
+            remover_peca(jogador);
+        if(jogador == 1)
+            jogador = 2;
+        else if(jogador == 2)
+            jogador = 1;
+    }
 
+    do{
+        mover_peca(jogador);
+        if(moinho_valido(jogador, pos))
+            remover_peca(jogador);
+        vencedor = verificar_vitoria();
+        if(jogador == 1)
+            jogador = 2;
+        else if(jogador == 2)
+            jogador = 1;
+    } while(vencedor == 0);
 
+    printf("\n\nO vencedor é o Jogador %d !!!\n\n", vencedor);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
+}
